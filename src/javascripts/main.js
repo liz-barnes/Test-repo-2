7aa6196cff3104845c27a33b1c367664ec3b0233
Liz <liz@Liz-s-Mac.local>
2020-09-02 21:19:47 -0500

    updated files

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,7 +1,7 @@
 import '../styles/main.scss';
 
 const init = () => {
-  $('#app').html('<h1>HELLO! You are up and running!</h1>');
+  $('#app').html('<h1>HELLO! This is your second repo!</h1>');
   console.log('YOU ARE UP AND RUNNING!');
 };
 
